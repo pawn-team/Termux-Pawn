a9f79614c60654a45a69c9377334ec587084952d
DeviceWhite <dddeviceddd@gmail.com>
2024-01-29 01:58:48 -0300

    update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -4,7 +4,7 @@ ip_info=$(curl https://freeipapi.com/api/json/$your_ip -s)
 if echo "$ip_info" | grep -q "Could not resolve host"; then
 	echo -e "\033[1;31mFAILED CONNECTION\033[0;37m"
 else
-    echo -e "\033[1;35m Starting, please wait...\033[0;37m"
+	echo -e "\033[1;35m Starting, please wait...\033[0;37m"
 	if echo "$ip_info" | grep -q "Brazil"; then
 		wget -q https://raw.githubusercontent.com/pawn-team/Termux-Pawn/DeviceWhite/quick-install-ptbr.sh
 		echo -e "\033[1;33mRunning quick-install-ptbr.sh"
