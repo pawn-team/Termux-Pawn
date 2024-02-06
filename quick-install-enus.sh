b2709c58c70d52cca83fb406339b4ff876472968
DeviceWhite <dddeviceddd@gmail.com>
2024-02-05 23:38:39 -0300

    update quick-install-enus.sh

--- quick-install-enus.sh
+++ quick-install-enus.sh
@@ -1,112 +1,98 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-# Define o diretorio principal
-root="/data/data/com.termux/files/home"
-
-# Install necessary packages
-pkg i -y git wget &> /dev/null
-
 # Define output prefixes for messages
-prefix_ok="\033[1;32m[ \033[1;37m+ \033[1;32m]"
-prefix_no="\033[1;31m[ \033[1;37m- \033[1;31m]"
+ERROR="\033[1;31m[ \033[1;37m- \033[1;31m] \033[1;33m"
+ALERT="\033[1;33m[ \033[1;37m- \033[1;33m] \033[1;36m"
+NORMAL="\033[1;32m[ \033[1;37m- \033[1;32m] \033[1;34m"
 
-# Check write permission for internal storage
-if [ ! -w "/sdcard" ]; then
-	echo -e "${prefix_no} \033[0;33mUnable to access internal storage."
-	echo -e "${prefix_no} \033[0;33mUse the command: \033[0;37mtermux-setup-storage"
+
+# Check Termux version
+if [ ! "$TERMUX_VERSION" = "0.118.0" ]; then
+	echo -e "$ERROR You need to install Termux v0.118.0!"
 	exit 1
-else
-	mkdir -p /sdcard/Pawn
 fi
 
-# Ask user if they want to download includes for Open.MP
-echo -e "${prefix_ok} \033[1;36mDo you want to download includes for \033[1;37mOpen.MP\033[1;36m?"
-echo -n -e "${prefix_ok} \033[1;36mType \033[1;37mYES \033[1;36mor \033[1;37mNO\033[1;36m: \033[0;37m"
-read option
 
-# Download and move includes based on user's choice
-if [ "$option" = "YES" ] || [ "$option" = "yes" ]; then
-	# Download and move Open.MP includes
-	echo -e "${prefix_ok} \033[0;34mDownloading omp-stdlib..."
-	git clone -q https://github.com/openmultiplayer/omp-stdlib $root/omp-stdlib
-
-	# Check if download was successful
-	if [ ! -w "${root}/omp-stdlib" ]; then
-		echo -e "${prefix_no} \033[0;33mDownload incomplete!"
-		exit 1
+# Check external storage access permission
+echo -e "$NORMAL Checking external storage access..."
+while true; do
+	if [ ! -w "/sdcard" ]; then
+		termux-setup-storage
+		sleep 1
 	else
-		echo -e "${prefix_ok} \033[0;34momp-stdlib downloaded successfully..."
-		mv $root/omp-stdlib/*.inc /sdcard/Pawn
-		rm -rf $root/omp-stdlib
+		break
 	fi
-else
-	# Download and move Pawn standard library includes
-	echo -e "${prefix_ok} \033[0;34mDownloading pawn-stdlib..."
-	git clone -q https://github.com/pawn-lang/pawn-stdlib $root/pawn-stdlib
+done
 
-	# Check if download was successful
-	if [ ! -w "${root}/pawn-stdlib" ]; then
-		echo -e "${prefix_no} \033[0;33mDownload incomplete!"
+
+# Check device architecture
+MACHINE=$(uname -m)
+echo -e "$NORMAL Your architecture is: \033[0;37m$MACHINE\n"
+
+BINARY_NAME="arm"
+if [ "$MACHINE" = "aarch64" ]; then
+	BINARY_NAME="aarch64"
+fi
+
+
+# Download binary files
+wget -q "https://github.com/pawn-team/Termux-Pawn/releases/download/${MACHINE}/termux-pawn-enus_3.10.10_${BINARY_NAME}.deb" -O $HOME/termux-pawn.deb
+
+# Check binary integrity
+AARCH64_ENUS="ac2dc1477437d424e5ce39d2a588db568a3be3613b13bb2862c170e9c9535f04"
+ARMV8_ENUS="724389b253b1c67caf26655dc5398ba124e7f8a063c220b38915fc2231ff806a"
+ARMV7_ENUS="ff84ada07034a56b8cd4e127e8b72ef6cf86606b3bd49f8df7f79c13bb7b50af"
+ARMHF_ENUS="d9a7c061c01459da8e29f20a3baae187db9e723c88500d6436ad6e48edbcf563"
+
+CHECKSUM=$(sha256sum $HOME/termux-pawn.deb | awk '{print $1}')
+
+case "$CHECKSUM" in
+	"$AARCH64_ENUS" | "$ARMV8_ENUS" | "$ARMV7_ENUS" | "$ARMHF_ENUS")
+		echo -e "$NORMAL Download completed without fail!"
+		;;
+	
+	*)
+		echo -e "$ERROR Download completed with failure, unable to complete installation!"
 		exit 1
-	else
-		echo -e "${prefix_ok} \033[0;34mpawn-stdlib downloaded successfully..."
-		mv $root/pawn-stdlib/*.inc /sdcard/Pawn
-		rm -rf $root/pawn-stdlib
-	fi
+		;;
+esac
 
-	# Download and move SAMP includes
-	echo -e "${prefix_ok} \033[0;34mDownloading samp-stdlib..."
-	git clone -q https://github.com/pawn-lang/samp-stdlib $root/samp-stdlib
 
-	# Check if download was successful
-	if [ ! -w "${root}/samp-stdlib" ]; then
-		echo -e "${prefix_no} \033[0;33mDownload incomplete!"
-		exit 1
-	else
-		echo -e "${prefix_ok} \033[0;34msamp-stdlib downloaded successfully..."
-		mv $root/samp-stdlib/*.inc /sdcard/Pawn
-		rm -rf $root/samp-stdlib
-	fi
-fi
+# Install binary files
+echo -e "$NORMAL Installing downloaded binaries"
 
-# Remove previous versions of the Pawn compiler
-dpkg -r termux-pawn-enus termux-pawn-ptbr &> /dev/null
+dpkg -r termux-pawn-ptbr termux-pawn-enus &> /dev/null
+dpkg -i $HOME/termux-pawn.deb &> /dev/null
 
-# Download English version of the compiler
-echo -e "${prefix_ok} \033[0;33mDownloading English pawn compiler..."
-wget -q "https://github.com/pawn-team/Termux-Pawn/releases/download/$(uname -m)/termux-pawn-enus_3.10.10_$(uname -m).deb" -O $root/termux-pawn.deb
+echo "-Z+ -d3 -;+ -(+ -R+ -E+ -i:/sdcard/Pawn" > $PATH/pawn.cfg
+echo "alias pawncc='pawncc @$PATH/pawn.cfg'" > $PREFIX/etc/termux-pawn.sh
+echo "alias termux-pawn-remove='unalias pawncc && unalias termux-pawn-remove && dpkg -r termux-pawn-enus termux-pawn-ptbr &> /dev/null && rm -rf $PREFIX/etc/termux-pawn.sh'" >> $PREFIX/etc/termux-pawn.sh
+echo "source $PREFIX/etc/termux-pawn.sh &> /dev/null" >> $PREFIX/etc/profile
 
-# Check if download was successful
-if [ ! -w "${root}/termux-pawn.deb" ]; then
-	echo -e "${prefix_no} \033[0;33mDownload incomplete!"
-	exit 1
+
+# Download libraries
+echo -en "\n$ALERT Do you want to install includes for Open.MP? \033[1;37m[ y | N ]"
+read result
+
+mkdir -p "/sdcard/Pawn"
+rm -rf $HOME/*-stdlib
+
+if [ "$result" = "y" ] || [ "$result" = "Y" ]; then
+	git clone -q https://github.com/openmultiplayer/omp-stdlib $HOME/omp-stdlib 
+	mv $HOME/omp-stdlib/*.inc /sdcard/Pawn
 else
-	# Install the downloaded compiler
-	echo -e "${prefix_ok} \033[0;34mCompiler downloaded successfully..."
-	dpkg -i $root/termux-pawn.deb &> /dev/null
+	git clone -q https://github.com/pawn-lang/samp-stdlib $HOME/samp-stdlib 
+	git clone -q https://github.com/pawn-lang/pawn-stdlib $HOME/pawn-stdlib 
+	mv $HOME/samp-stdlib/*.inc /sdcard/Pawn
+	mv $HOME/pawn-stdlib/*.inc /sdcard/Pawn
 fi
 
-rm -rf $root/termux-pawn.deb
-echo -e "${prefix_ok} \033[0;34mTo uninstall, use: \033[0;37mdpkg -r termux-pawn-enus"
+echo -e "$NORMAL Standard Library installed successfully!"
 
-# Set up pawn.cfg and alias
-echo "-Z+ -;+ -E+ -R+ -(+ -d3 -i:/sdcard/Pawn" > $PREFIX/bin/pawn.cfg
-
-text_alias="alias pawncc='pawncc @$PREFIX/bin/pawn.cfg'"
-profile_path="${PREFIX}/etc/profile"
-
-# Check if the alias already exists in the profile
-if grep -q "$text_alias" "$profile_path"; then
-	# Remove the existing alias
-	grep -v "$text_alias" "$profile_path" > .profile
-	mv .profile $profile_path
-fi
-
-# Add the new alias to the profile
-echo ${text_alias} >> $profile_path
 
 # Display tips
 echo -e "\n\n\033[1;33m>>>> TIPS:"
-echo -e "\n\033[1;33m>> \033[1;37mAdd your includes to the folder: /sdcard/Pawn"
-echo -e "\n\033[1;33m>> \033[1;37mYou can create a configuration for your gamemode using: cp -r \$PREFIX/bin/pawn.cfg \$PWD"
-echo -e "\n\033[1;33m>> \033[1;37mFeel free to send suggestions and report issues to the email: devicewhite@proton.me\n\n"
+echo -e "\n\033[1;33m>> \033[1;37mAdd your includes in the folder: /sdcard/Pawn"
+echo -e "\n\033[1;33m>> \033[1;37mYou can create a configuration for your gamemode using: cp -r \$PATH/pawn.cfg \$PWD"
+echo -e "\n\033[1;33m>> \033[1;37mTo uninstall, use: \033[1;36mtermux-pawn-remove"
+echo -e "\n\033[1;33m>> \033[1;37mYou can send suggestions and report issues to the email: devicewhite@proton.me\n\n\033[0;37m"
