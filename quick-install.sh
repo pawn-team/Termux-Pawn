b2a3dde6244736f01c11405830190216eb48937f
DeviceWhite <dddeviceddd@gmail.com>
2024-02-11 01:45:56 -0300

    update quick-install.sh

--- quick-install.sh
+++ quick-install.sh
@@ -33,11 +33,13 @@ machine=$(uname -m)
 case "$machine" in
 	"armv7l" | "armv8l" | "armhf")
 		wget -q --no-check-certificate https://github.com/pawn-team/Termux-Pawn/releases/download/$machine/termux-pawn-$1_3.10.10_arm.deb
+		termux-pawn-remove &> /dev/null
 		dpkg -i termux-pawn-$1_3.10.10_arm.deb &> /dev/null
 		;;
 	
 	"aarch64")
 		wget -q --no-check-certificate https://github.com/pawn-team/Termux-Pawn/releases/download/aarch64/termux-pawn-$1_3.10.10_aarch64.deb
+		termux-pawn-remove &> /dev/null
 		dpkg -i termux-pawn-$1_3.10.10_aarch64.deb &> /dev/null
 		;;
 	
